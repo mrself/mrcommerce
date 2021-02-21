d545104b6608864ed97446b5a72440572502a147
Lightman <yaruk.my@gmail.com>
2021-02-21 18:08:18 +0200

    configure classes required for Symfony bundle

---
+++ src/DependencyInjection/MrselfMrcommerceExtension.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Mrself\Mrcommerce\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class MrselfMrcommerceExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+    }
+}
---
+++ src/MrcommerceBundle.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Mrself\Mrcommerce;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MrcommerceBundle extends Bundle
+{
+
+}
