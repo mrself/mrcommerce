6def75591561e3ad1c816affcf122d79dab42f10
Lightman <yaruk.my@gmail.com>
2021-03-08 12:17:49 +0200

    create CategoryImporter

---
+++ src/Import/BC/Catalog/Category/CategoryImporter.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Mrself\Mrcommerce\Import\BC\Catalog\Category;
+
+use Mrself\Mrcommerce\Import\BC\Catalog\AbstractImporter;
+use Mrself\Mrcommerce\Import\BC\Catalog\ImporterInterface;
+
+class CategoryImporter extends AbstractImporter implements ImporterInterface
+{
+    protected function getMethodSingle(): string
+    {
+        return 'getCategories';
+    }
+
+    protected function getMethodMultiple(): string
+    {
+        return 'getCategoryById';
+    }
+
+}
